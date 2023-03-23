71c8a5054508c74524497a76154d8f9d2f9a95a9
BlackVVell <tatarchuk2000@gmail.com>
2023-03-23 12:36:46 +0200

    Lab2 - task 2

---
+++ CounterHomework/app/src/main/java/it_school/sumdu/edu/ua/counterhomework/MainActivity.java
@@ -0,0 +1,42 @@
+package it_school.sumdu.edu.ua.counterhomework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    private TextView countText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        countText = findViewById(R.id.counter);
+        Button countButton = findViewById(R.id.button);
+        countButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                countText.setText(Integer.toString(count));
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("count", count);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        count = savedInstanceState.getInt("count");
+        countText.setText(Integer.toString(count));
+    }
+}
